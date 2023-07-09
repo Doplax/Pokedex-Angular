4a9cabc5fa274de00350837057b075c98036b2a7
Doplax <doplax@gmail.com>
2023-07-09 19:03:51 +0200

    export class PokemonList {   name: string;   url:string | null;
      constructor(){
        this.name = "";
        this.url = null;
      }
    }

--- pokedex/src/app/services/pokemon.service.ts
+++ pokedex/src/app/services/pokemon.service.ts
@@ -3,20 +3,21 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { PokemonList } from '../models/pokemon.list'
 import { PokemonDetail } from '../models/pokemon.details'
+import { map } from "rxjs/operators";
+
 
 @Injectable({providedIn: 'root'})
 export class PokemonService {
-  private baseUrl = 'https;//pokeapi.co/api/v2'
+  private baseUrl = 'https://pokeapi.co/api/v2'
 
   constructor( private http: HttpClient) {
   }
 
-
   getPokemonList(offset: number, limit: number = 20): Observable<PokemonList[]>  {
-    return this.http.get<PokemonList[]>(`${this.baseUrl}?offset=${offset}&limit=${limit}`);
+    return this.http.get<PokemonList[]>(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`);
   }
 
   getPokemonDetail(pokemon: number | string): Observable<PokemonDetail> {
-    return this.http.get<PokemonDetail>(`${this.baseUrl}?pokemon/${pokemon}`)
+    return this.http.get<PokemonDetail>(`${this.baseUrl}/pokemon/${pokemon}`)
   }
 }
